baf34ba31a8d353e8e5a71a392ee8ffc663aa1be
Leung <charlottexprojects@gmail.com>
2021-04-19 10:37:47 +0800

    add spawn object function

---
+++ VRMiniProject/Assets/Script/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    AudioSource audioSource = null;
+    public Transform spawnPosition = null;
+    public List<GameObject> prefabsList = new List<GameObject>();
+    public List<AudioClip> soundList = new List<AudioClip>();
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void SpawnToolsAgain(int typeNumber)
+    {
+        audioSource.PlayOneShot(soundList[0]);
+        Instantiate(prefabsList[typeNumber], spawnPosition);
+    }
+}
---
+++ VRMiniProject/Assets/Script/ObjectPosition.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPosition : MonoBehaviour
+{
+    AudioSource audioSource = null;
+    [SerializeField] int myNumber = 0;
+    bool hit = false;
+    bool took = false;
+    GameManager gameManager = null;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        gameManager = GameObject.Find("GameManager").gameObject.transform.GetComponent<GameManager>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (took)
+        {
+            audioSource.Play();
+            took = false;
+        }
+
+        if ((other.gameObject.name == "Terrain" || other.gameObject.name == "WorldBottom") && !hit)
+        {
+            if (Application.isEditor) Debug.Log($"{name} hit the ground");
+            hit = true;
+            gameManager.SpawnToolsAgain(myNumber);
+        }
+    }
+
+    public void TookObject()
+    {
+
+    }
+
+    IEnumerator _TookObject()
+    {
+        yield return new WaitForSeconds(1);
+        took = true;
+    }
+}
