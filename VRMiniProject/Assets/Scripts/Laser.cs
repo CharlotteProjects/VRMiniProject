8033dea383ccead88e7865bc09c6e9344ce6eecd
Leung <charlottexprojects@gmail.com>
2021-04-23 23:08:18 +0800

    add laser

---
+++ VRMiniProject/Assets/Scripts/Laser.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+
+	LineRenderer lr;
+	Material mat;
+
+	[SerializeField]
+	Texture2D normal, hit;
+    [SerializeField]
+    GameObject point;
+    [SerializeField]
+    Color pointColorOff, pointColorOn;
+
+    Material pointMat;
+    public GameObject _point { get { return point; } }
+    float range;
+
+	bool bhit;
+
+	void Awake(){
+		lr = GetComponent<LineRenderer> ();
+		mat = lr.material;
+        pointMat = point.GetComponent<Renderer>().material;
+
+    }
+
+    public void SetRange(float input)
+    {
+        range = input;
+    }
+
+    public void OnOff(bool input)
+    {
+        lr.enabled = input;
+        point.SetActive(input);
+    }
+
+	public void SetHitPont(Vector3 input){
+		Vector3 temp = transform.InverseTransformPoint (input);
+
+        point.SetActive(true);
+        point.transform.position = input;
+        point.transform.LookAt(Valve.VR.InteractionSystem.Player.instance.hmdTransform);
+
+
+        lr.SetPosition (1, temp);
+	}
+
+	public void SetNoHitPoint(){
+        point.SetActive(false);
+
+        lr.SetPosition (1, new Vector3(0f, 0f, range));
+	}
+
+	public void SetTextue(bool input){
+		if (bhit == input)
+			return;
+
+		mat.mainTexture = input ? hit : normal;
+        //		mat.SetTexture ("_EmissionMap", input ? hit : normal);
+        pointMat.color = input ? pointColorOn : pointColorOff;
+
+        bhit = input;
+	}
+
+
+}
---
+++ VRMiniProject/Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour
+{
+
+    [SerializeField]
+    float range = 0.3f;
+
+    [SerializeField]
+    Laser l;
+
+    Valve.VR.InteractionSystem.Hand _hand;
+
+    GameObject selectingObj;
+
+    // Use this for initialization
+    void Start()
+    {
+        _hand = GetComponent<Valve.VR.InteractionSystem.Hand>();
+
+
+        l.SetRange(range);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_hand.hoverLocked && !_hand.hoveringInteractable)
+        {
+            l.OnOff(true);
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, range))
+            {
+                l.SetHitPont(hit.point);
+//                if (hit.collider.GetComponent<Btn>())
+//                {
+                    if (selectingObj != hit.collider.gameObject)
+                    {
+                        if (selectingObj != null)
+                            selectingObj.SendMessage("OnHandHoverEnd", _hand, SendMessageOptions.DontRequireReceiver);
+
+                        selectingObj = hit.collider.gameObject;
+
+                        selectingObj.SendMessage("OnHandHoverBegin", _hand, SendMessageOptions.DontRequireReceiver);
+                    }
+                    if (selectingObj != null)
+                        selectingObj.SendMessage("HandHoverUpdate", _hand, SendMessageOptions.DontRequireReceiver);
+
+                    l.SetTextue(true);
+                    /*                if (_hand.GetStandardInteractionButtonDown())
+                                    {
+                                        selectingObj.SendMessage("OnHandHoverBegin", _hand);
+                                    }*/
+/*                }
+                else
+                {
+                    if (selectingObj != null)
+                        selectingObj.SendMessage("OnHandHoverEnd", _hand, SendMessageOptions.DontRequireReceiver);
+                    selectingObj = null;
+
+                    l.SetTextue(false);
+                }*/
+            }
+            else
+            {
+                if (selectingObj != null)
+                    selectingObj.SendMessage("OnHandHoverEnd", _hand, SendMessageOptions.DontRequireReceiver);
+                selectingObj = null;
+
+                l.SetTextue(false);
+                l.SetNoHitPoint();
+            }
+        }
+        else
+        {
+            l.OnOff(false);
+        }
+
+    }
+}
